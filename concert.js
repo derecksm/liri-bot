39d3d35d07cf7476a093a1ff3101ed21e98a87b4
Dereck San Miguel <derecksanmiguel@dhcp-v127-193.mobile.uci.edu>
2019-04-25 18:34:51 -0700

    Updated concert.js

--- concert.js
+++ concert.js
@@ -4,17 +4,12 @@ const moment = require('moment')
 
 
 let concertSearch = (concertFinder) => {
-
-  let concertSrch = _ => {
-    if (concertFinder.join(' ').length < 1) {
-      return "Sorry we couldn't find the concert"
-    } else {
-      return concertFinder.join(' ')
-    }
-  }
  
-  axios.get(`https://rest.bandsintown.com/artists/${concertSrch()}/events?app_id=codingbootcamp`)
+  axios.get(`https://rest.bandsintown.com/artists/${concertFinder[0]}/events?app_id=codingbootcamp`)
     .then(({ data }) => {
+      if (!data.length) {
+        console.log('No Concerts were found for that band.')
+      } else {
       let venueName = data[0].venue.name
       let venueCity = data[0].venue.city
       let venueCountry = data[0].venue.country
@@ -24,6 +19,7 @@ let concertSearch = (concertFinder) => {
       Location: ${venueCity},${venueCountry}
       Date: ${date}
       `)
+      }
     })
     .catch(e => console.log(e))
 }
